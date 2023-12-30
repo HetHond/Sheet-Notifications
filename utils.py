27965603bb45fbd38c69d1351d38af21ca62da0c
Senna de Vos <hethond2020@outlook.com>
2023-12-30 20:41:41 +0100

    Improved 'flatten' func

--- utils.py
+++ utils.py
@@ -1,12 +1,24 @@
+from collections.abc import Iterable
+
+
 def euro_float(value: str) -> float:
     return float(value.replace(',', '.'))
 
 
-def flatten(input_list):
-    flat_list = []
-    for item in input_list:
-        if isinstance(item, list):
-            flat_list.extend(flatten(item))
+def flatten(input_arr, output_arr=None):
+    if output_arr is None:
+        output_arr = []
+    for item in input_arr:
+        if not isinstance(item, str) and isinstance(item, Iterable):
+            flatten(item, output_arr)
         else:
-            flat_list.append(item)
-    return flat_list
+            output_arr.append(item)
+    return output_arr
+
+
+def flatten_iter(iterable):
+    for element in iterable:
+        if not isinstance(element, str) and isinstance(element, Iterable):
+            yield from flatten_iter(element)
+        else:
+            yield element
